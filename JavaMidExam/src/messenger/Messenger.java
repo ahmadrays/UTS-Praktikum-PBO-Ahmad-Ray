6925247969e17df575b3bb87770220cef65e4c23
ahmadrays <99309030+ahmadrays@users.noreply.github.com>
2022-03-23 18:02:51 +0700

    Add files via upload

---
+++ JavaMidExam/src/messenger/Messenger.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package messenger;
+
+/**
+ *
+ * @author Ahmad Ray
+ */
+public interface Messenger {
+    void sendMessage(String receiver, String subject, String messege);
+}
